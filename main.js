84d5428afb79c4eb31680729d78684c3097d7093
sssil <sssil@gmail.com>
2021-01-27 11:44:19 +0900

    resize manage

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let t = 0.0;
 let screen_size;
 
 window.onload = function start() {
+
     if (!(gl = glInit())) return;
 
     program = new Program(gl,
@@ -78,30 +79,24 @@ function initializeAttributes(gl) {
     vao = new VAO(gl, program._id, vertices, texcoord, indices);
 }
 
-function glInit() {
-    const canvas = document.querySelector("#glCanvas");
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
-    canvas.width = window.innerWidth * 9 / 10;
-    canvas.height = window.innerHeight * 9 / 10;
+let canvas, text;
 
-    if (canvas.width < canvas.height) {
-        console.log("canvas.width : " + canvas.width);
-        canvas.height = canvas.width * 9 / 16;
-    } else {
-        console.log("canvas.height : " + canvas.height);
-        canvas.width = canvas.height * 16 / 9;
-    }
+function glInit() {
+    canvas = document.querySelector("#glCanvas");
+    text = document.querySelector("p");
+
 
     screen_size = glMatrix.vec2.fromValues(canvas.width, canvas.height);
     console.log("screen size: " + screen_size[0] + " * " + screen_size[1]);
 
     const gl = canvas.getContext("webgl");
     if (!gl) {
-        const paragraph = document.querySelector("p");
-        paragraph.innerHTML = "Failed to get WebGL context." + "Your browser or device may not support WebGL.";
+        text.innerHTML = "Failed to get WebGL context." + "Your browser or device may not support WebGL.";
         return null;
     }
+
+    resize();
+
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.lineWidth(1.0);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -111,4 +106,22 @@ function glInit() {
     return gl;
 }
 
+function resize(event) {
+    let width = (window.innerWidth) * 0.9;
+    let height = (window.innerHeight - text.clientHeight) * 0.9;
 
+    let needed_height = width * 9 / 16;
+
+    if (height > needed_height) {
+        height = needed_height;
+    } else {
+        width = height * 16 / 9;
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+    const gl = canvas.getContext("webgl");
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+}
+
+window.addEventListener('resize', resize);
