bf0063bba113bbfd67a06aab9ce166a7f21400a1
sssil <sssil@gmail.com>
2021-01-28 12:21:05 +0900

    screen size fixed

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ let canvas, text;
 
 window.onload = function () {
     canvas = document.querySelector("#glCanvas");
-    // text = document.querySelector("#glTitle");
 
     window.onresize(undefined);
     console.log("screen size: " + canvas.width + " * " + canvas.height);
@@ -16,7 +15,7 @@ window.onload = function () {
 
 window.onresize = function () {
     let width = (window.innerWidth) * 0.9;
-    let height = (window.innerHeight /*- text.clientHeight*/) * 0.9;
+    let height = (window.innerHeight) * 0.9;
 
     let needed_height = width * 9 / 16;
 
@@ -37,7 +36,7 @@ window.onresize = function () {
 };
 
 window.onkeydown = function (e) {
-
+    console.log(e);
 };
 
 document.querySelector("#lineButton").onclick = function () {
