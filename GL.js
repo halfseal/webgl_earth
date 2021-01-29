bc6a6d375ea710d9d76af98651704f6da645ba3a
sssil <sssil@gmail.com>
2021-01-29 13:22:54 +0900

    changing image path

--- GL.js
+++ GL.js
@@ -72,7 +72,7 @@ export class GL {
 
         vao2 = new VO(gl, program.id, vertices, normal, texcoord, indices);
 
-        softball_tex = new Texture(gl, "./source/image/earthmap1k.jpg", true);
+        softball_tex = new Texture(gl, "earthmap1k.jpg", true);
 
         window.requestAnimationFrame(loop);
     }
